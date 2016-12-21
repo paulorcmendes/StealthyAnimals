0789a78b0ec6d20d1d791f1f241978af07001b0d
paulorcmendes <mendesdpn@gmail.com>
2016-12-21 17:49:34 -0300

    Lendo Arquivo

---
+++ Assets/scripts/ReadLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadLevel : MonoBehaviour {
+	public string txtFile;
+	// Use this for initialization
+	void Start () {
+		string[] map = System.IO.File.ReadAllText (txtFile).Split (',');
+		//System.IO.File.WriteAllText ("teste.txt", "pedro eh bundao");
+		Debug.Log (map[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
