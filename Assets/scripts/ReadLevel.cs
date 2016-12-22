cabb84bdac988a7ed544d6429743ec0de83d2092
paulorcmendes <mendesdpn@gmail.com>
2016-12-22 01:15:17 -0300

    Lendo texto e gerando mapa

--- Assets/scripts/ReadLevel.cs
+++ Assets/scripts/ReadLevel.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class ReadLevel : MonoBehaviour {
 	public string txtFile;
-	// Use this for initialization
-	void Start () {
-		string[] map = System.IO.File.ReadAllText (txtFile).Split (',');
-		//System.IO.File.WriteAllText ("teste.txt", "pedro eh bundao");
-		Debug.Log (map[0]);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public string[] ReadMap(){
+		string txt = Regex.Replace (System.IO.File.ReadAllText (txtFile), @"\s+", string.Empty);
+		return txt.Split (',');
 	}
 }
--- Assets/scripts/TerrainGenerator.cs
+++ Assets/scripts/TerrainGenerator.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 0 - Grass
+ * 1 - Sand
+ * 2 - Rock
+ * 3 - Sand
+ */
 
 public class TerrainGenerator : MonoBehaviour {
 	public GameObject grassPrefab;
 	public GameObject sandPrefab;
 	public GameObject rockPrefab;
 	public GameObject boxPrefab;
-	public enum TerrainType{Grass, Rock, Box, Sand};
+	public enum TerrainType{Grass, Sand, Rock, Box};
 	public TerrainType[,] terrainMatrix;
-	public uint terrainLength;
-	public uint terrainHeight;
+	public uint terrainColumns;
+	public uint terrainRows;
 
 	// Use this for initialization
 	void Start () {
@@ -24,32 +29,45 @@ public class TerrainGenerator : MonoBehaviour {
 		
 	}
 	void CreateTerrain(){
-		this.terrainMatrix = new TerrainType[terrainLength,terrainHeight];
-		for (int i = 0; i < terrainLength; i++) {
-			for (int j = 0; j < terrainHeight; j++) {
-				if ((i + j) % 2 == 0) {
-					terrainMatrix[i,j] = TerrainType.Rock;
-				} else {
-					terrainMatrix[i,j] = TerrainType.Sand;
+		int indexTxtMap = 0;
+		terrainMatrix = new TerrainType[terrainRows, terrainColumns];
+		string[] txtMap = GetComponent<ReadLevel> ().ReadMap ();
+		for (int i = 0; i < terrainRows; i++) {
+			for (int j = 0; j < terrainColumns; j++) {
+				switch (txtMap [indexTxtMap++]) {
+				case "0":
+					terrainMatrix [(terrainRows-1)-i, j] = TerrainType.Grass;
+					break;
+				case "1":
+					terrainMatrix [(terrainRows-1)-i, j] = TerrainType.Sand;
+					break;
+				case "2":
+					terrainMatrix [(terrainRows-1)-i, j] = TerrainType.Rock;
+					break;
+				case "3":
+					terrainMatrix [(terrainRows-1)-i, j] = TerrainType.Box;
+					break;
 				}
 			}
 		}
 	}
 	void DrawTerrain(){
-		for (int i = 0; i < terrainLength; i++) {
-			for (int j = 0; j < terrainHeight; j++) {
+		//x é coluna - j
+		//y é linha - i
+		for (int i = 0; i < terrainRows; i++) {
+			for (int j = 0; j < terrainColumns; j++) {
 				switch(terrainMatrix[i,j]){
 				case TerrainType.Grass:
-					grassPrefab.Spawn (new Vector3 (i, j, 0));
+					grassPrefab.Spawn (new Vector3 (j, i, 0));
 					break;
 				case TerrainType.Sand:
-					sandPrefab.Spawn (new Vector3 (i, j, 0));
+					sandPrefab.Spawn (new Vector3 (j, i, 0));
 					break;
 				case TerrainType.Rock:
-					rockPrefab.Spawn (new Vector3(i, j, 0));
+					rockPrefab.Spawn (new Vector3(j, i, 0));
 					break;
 				case TerrainType.Box:
-					boxPrefab.Spawn (new Vector3 (i, j, 0));
+					boxPrefab.Spawn (new Vector3 (j, i, 0));
 					break;
 				}
 			}
