f2ac395ae98b106cf60e36c3a33242b7898103b6
ylderlan <ylderlanfy@hotmail.com>
2016-12-23 16:24:22 -0300

    menu inicial

--- Assets/scripts/MenuController.cs
+++ Assets/scripts/MenuController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class MenuController : MonoBehaviour {
 
 	// Use this for initialization
