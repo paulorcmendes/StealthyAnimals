a1eec9122bd9b9f9384f8df321228cf1a4a076c5
paulorcmendes <mendesdpn@gmail.com>
2017-01-05 11:17:33 -0300

    adicionado gamestate do player; colisão com guardas; detecção de vitória

---
+++ Assets/scripts/GameStatePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State{escaping, captured, free}
+public class GameStatePlayer : MonoBehaviour {
+	private State myState;
+	// Use this for initialization
+	void Start () {
+		this.myState = State.escaping;	
+	}
+
+	public State MyState 
+	{
+		get 
+		{ 
+			return myState; 
+		}
+	}
+	public void freePlayer(){
+		if (MyState == State.escaping)
+			this.myState = State.free;
+	}
+	public void capturePlayer(){
+		if (MyState == State.escaping)
+			this.myState = State.captured;
+	}
+}
--- Assets/scripts/MovePlayer.cs
+++ Assets/scripts/MovePlayer.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlayer : MatrixMovement{
-	
+	private GameStatePlayer gameStatePlayer;
 	// Use this for initialization
 	protected override void ChildStart ()
 	{
-		//
+		gameStatePlayer = GetComponent<GameStatePlayer> ();
 	}
 	
 	// Update is called once per frame
@@ -15,7 +15,7 @@ public class MovePlayer : MatrixMovement{
 		Move ();	
 	}
 	void Move(){
-		if(Input.anyKey){
+		if(Input.anyKeyDown && gameStatePlayer.MyState == State.escaping){
 			Vector2 translation = Vector2.zero;
 			if (Input.GetKeyDown("a")) {
 				translation = new Vector2 (-1.0f, 0.0f);
@@ -28,8 +28,21 @@ public class MovePlayer : MatrixMovement{
 			}		
 			if (CanMove (currentPosition + translation)) {
 				currentPosition += translation; 
+				haveIWon ();
 			}
 			transform.position = currentPosition + offset;
+		}
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.CompareTag ("Guard")) {
+			Debug.Log ("cap");
+			gameStatePlayer.capturePlayer ();
+		}			
+	}
+	void haveIWon(){
+		if (terrainGenerator.terrainMatrix [(int)currentPosition.y, (int)currentPosition.x] == TerrainType.Exit) {
+			//gameStatePlayer.freePlayer ();
+			Debug.Log("win");
 		}
 	}
 }
