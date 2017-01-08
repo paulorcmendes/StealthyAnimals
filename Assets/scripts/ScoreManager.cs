a9d3ed048c233061e57f3564a60073e3da53cc6b
ylderlan <ylderlanfy@hotmail.com>
2017-01-08 14:54:50 -0300

    Adicionado algumas funcoes do score

---
+++ Assets/scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int score;
+	public Text scoreT;
+
+	// Update is called once per frame
+	void Update () {
+		scoreT.text = score.ToString ();
+	}
+}
